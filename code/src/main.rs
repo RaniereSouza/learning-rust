09a854c4d0e41bf29d45d3c3039e2c83d49c459f
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-01-11 06:23:10 +0000

    Getting rid of the nested match hell in the main function

--- code/src/main.rs
+++ code/src/main.rs
@@ -5,21 +5,9 @@ mod fibonacci; use fibonacci::fibonacci_rec;
 
 fn main() -> ExitCode {
   println!("insert an integer n to get the nth Fibonacci number: ");
-  return match read_line() {
-    Some(input) => {
-      return match text_to_number::<i32>(input) {
-        Some(number) => {
-          match fibonacci_rec(number.clone()) {
-            Some(result) => {
-              println!("the #{number} Fibonacci number is {result}");
-              return ExitCode::SUCCESS;
-            },
-            None => ExitCode::FAILURE,
-          }
-        },
-        None => ExitCode::FAILURE,
-      };
-    },
-    None => ExitCode::FAILURE,
-  };
+  let Some(input) = read_line() else { return ExitCode::FAILURE };
+  let Some(number) = text_to_number::<i32>(input) else { return ExitCode::FAILURE };
+  let Some(result) = fibonacci_rec(number) else { return ExitCode::FAILURE };
+  println!("the #{number} Fibonacci number is {result}");
+  return ExitCode::SUCCESS;
 }

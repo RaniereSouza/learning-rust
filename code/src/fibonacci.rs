5ac94b547445908e4d186c7b5afbc37994d7b83b
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-01-11 22:09:02 +0000

    Introducing first unit tests for the fibonacci module

--- code/src/fibonacci.rs
+++ code/src/fibonacci.rs
@@ -11,3 +11,46 @@ pub fn fibonacci_rec(n: i32) -> Option<i32> {
     },
   };
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn should_return_one() {
+    // Arrange
+    let input1 = 0; let input2 = 1;
+    // Act
+    let result1 = fibonacci_rec(input1);
+    let result2 = fibonacci_rec(input2);
+    // Assert
+    assert_eq!(result1, Some(1));
+    assert_eq!(result2, Some(1));
+  }
+
+  #[test]
+  fn should_correctly_compute_fibonacci() {
+    // Arrange
+    let input1 = 2; let input2 = 5; let input3 = 9; let input4 = 13;
+    // Act
+    let result1 = fibonacci_rec(input1);
+    let result2 = fibonacci_rec(input2);
+    let result3 = fibonacci_rec(input3);
+    let result4 = fibonacci_rec(input4);
+    // Assert
+    assert_eq!(result1, Some(2));
+    assert_eq!(result2, Some(8));
+    assert_eq!(result3, Some(55));
+    assert_eq!(result4, Some(377));
+  }
+
+  #[test]
+  fn should_not_accept_negative_input() {
+    // Arrange
+    let input1 = -5;
+    // Act
+    let result1 = fibonacci_rec(input1);
+    // Assert
+    assert_eq!(result1, None);
+  }
+}

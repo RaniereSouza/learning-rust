ed4383e94a1c30f75b12f3836c6ed21e6df7dec8
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-01-11 06:36:41 +0000

    Taking away unnecessary bracket wrap and return keyword from the fibonacci_rec function default match arm

--- code/src/fibonacci.rs
+++ code/src/fibonacci.rs
@@ -5,11 +5,9 @@ pub fn fibonacci_rec(n: i32) -> Option<i32> {
       return None;
     },
     0 | 1 => Some(1),
-    _ => {
-      return match (fibonacci_rec(n - 1), fibonacci_rec(n - 2)) {
-        (a, b) if a.is_none() || b.is_none() => None,
-        (a, b) => Some(a.unwrap() + b.unwrap()),
-      };
+    _ => match (fibonacci_rec(n - 1), fibonacci_rec(n - 2)) {
+      (a, b) if a.is_none() || b.is_none() => None,
+      (a, b) => Some(a.unwrap() + b.unwrap()),
     },
   };
 }

8653f68d415d8a84da16485807aa8bdfefae5b8a
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-01-13 12:54:09 +0000

    Introducing unit tests to the helpers module; Changing the function read_line into read_line_from to make it testable

--- code/src/helpers.rs
+++ code/src/helpers.rs
@@ -1,4 +1,4 @@
-use std::{ops::Add, str::FromStr, any::type_name, io::stdin, fmt::Debug};
+use std::{ops::Add, str::FromStr, any::type_name, io::BufRead, fmt::Debug};
 
 /// Custom trait to basically represent *any numeric type*:
 /// * Can be passed onto the add operation (`+`)
@@ -9,22 +9,62 @@ impl<T: Add + FromStr> Numeric for T {}
 pub fn text_to_number<T>(text: String)  -> Result<T, String> where
   T: Numeric, <T as FromStr>::Err: Debug,
 {
-  let clean_text = text.trim();
-  return match clean_text.parse::<T>() {
+  return match text.parse::<T>() {
     Ok(number) => Ok(number),
     Err(error) => Err(format!(
-      "[ERROR] could not parse value \"{clean_text}\" into a {} (NaN):\n{:?}",
+      "[ERROR] could not parse value \"{text}\" into a {} (NaN):\n{:?}",
       type_name::<T>(), error,
     )),
   };
 }
 
-pub fn read_line() -> Result<String, String> {
+pub fn read_line_from<R: BufRead>(mut stream: R) -> Result<String, String> {
   let mut buffer = String::new();
-  return match stdin().read_line(&mut buffer) {
-    Ok(_) => Ok(buffer),
+  return match stream.read_line(&mut buffer) {
+    Ok(_) => Ok(format!("{}", buffer.trim())),
     Err(error) => Err(format!(
-      "[ERROR] could not read input from user:\n{:?}", error,
+      "[ERROR] could not read input content:\n{:?}", error,
     )),
   };
+}
+
+#[cfg(test)]
+mod tests {
+  use std::io::Cursor;
+  use super::*;
+
+  #[test]
+  fn should_correctly_convert_text_to_number() {
+    // Arrange
+    let input1 = format!("42"); let input2 = format!("-13");
+    let input3 = format!("-001000"); let input4 = format!("3.1416");
+    let input5 = format!("3000000000"); let input6 = format!("06.0022140760");
+    // Act
+    let result1 = text_to_number::<u8>(input1);
+    let result2 = text_to_number::<i8>(input2);
+    let result3 = text_to_number::<i16>(input3);
+    let result4 = text_to_number::<f32>(input4);
+    let result5 = text_to_number::<u64>(input5);
+    let result6 = text_to_number::<f64>(input6);
+    // Assert
+    assert_eq!(result1, Ok(42u8));
+    assert_eq!(result2, Ok(-13i8));
+    assert_eq!(result3, Ok(-1000i16));
+    assert_eq!(result4, Ok(3.1416f32));
+    assert_eq!(result5, Ok(3000000000u64));
+    assert_eq!(result6, Ok(6.0022140760f64));
+  }
+
+  #[test]
+  fn should_read_correctly_from_input() {
+    // Arrange
+    let input1 = Cursor::new("Hello, World!");
+    let input2 = Cursor::new("Hello, Rust!\nHello World!\n");
+    // Act
+    let result1 = read_line_from(input1);
+    let result2 = read_line_from(input2);
+    // Assert
+    assert_eq!(result1, Ok("Hello, World!".to_string()));
+    assert_eq!(result2, Ok("Hello, Rust!".to_string()));
+  }
 }
--- code/src/main.rs
+++ code/src/main.rs
@@ -1,6 +1,6 @@
-use std::{process::ExitCode, fmt::Display};
+use std::{fmt::Display, process::ExitCode, io::{stdin, BufReader}};
 
-mod helpers; use helpers::{read_line, text_to_number};
+mod helpers; use helpers::{read_line_from, text_to_number};
 mod fibonacci; use fibonacci::fibonacci_rec;
 
 fn log_err_return_failure<T, E: Display>(res: Result<T, E>) -> ExitCode {
@@ -10,10 +10,14 @@ fn log_err_return_failure<T, E: Display>(res: Result<T, E>) -> ExitCode {
 fn main() -> ExitCode {
   println!("insert an integer n to get the nth Fibonacci number: ");
 
+  let stdin_with_readline = BufReader::new(stdin());
   let mut input: Option<i32> = None;
-  let result = read_line()
+  let result =
+    read_line_from(stdin_with_readline)
     .and_then(text_to_number)
-    .and_then(|parsed_number| { input = Some(parsed_number); fibonacci_rec(parsed_number) });
+    .and_then(|parsed_number| {
+      input = Some(parsed_number); fibonacci_rec(parsed_number)
+    });
   if result.is_err() { return log_err_return_failure(result) };
 
   println!("the #{} Fibonacci number is {}", input.unwrap(), result.unwrap());

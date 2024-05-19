eb1318011f0f93a90435f7e726742e7221c74018
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-05-19 17:15:09 -0300

    Adding Slint to the project to make GUIs; First successful GUI using Slint, with button click and text input event handling

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,25 +1,46 @@
-use std::{process::ExitCode, io::{stdin, BufReader}};
+// use std::{process::ExitCode, io::{stdin, BufReader}};
 
-mod helpers; use helpers::{read_line_from, text_to_number};
-mod fibonacci; use fibonacci::fibonacci_memo;
+// mod helpers; use helpers::{read_line_from, text_to_number};
+// mod fibonacci; use fibonacci::fibonacci_memo;
 
-fn main() -> ExitCode {
-  println!("insert an integer n to get the nth Fibonacci number: ");
+// fn main() -> ExitCode {
+//   println!("insert an integer n to get the nth Fibonacci number: ");
 
-  let mut stdin_bufreader = BufReader::new(stdin());
-  let mut input: Option<i32> = None;
-  let result =
-    read_line_from(&mut stdin_bufreader)
-    .and_then(text_to_number)
-    .and_then(|parsed_number| {
-      input = Some(parsed_number); fibonacci_memo(parsed_number)
-    });
+//   let mut stdin_bufreader = BufReader::new(stdin());
+//   let mut input: Option<i32> = None;
+//   let result =
+//     read_line_from(&mut stdin_bufreader)
+//     .and_then(text_to_number)
+//     .and_then(|parsed_number| {
+//       input = Some(parsed_number); fibonacci_memo(parsed_number)
+//     });
 
-  if result.is_err() {
-    println!("{}", result.err().unwrap());
-    return ExitCode::FAILURE;
-  };
+//   if result.is_err() {
+//     println!("{}", result.err().unwrap());
+//     return ExitCode::FAILURE;
+//   };
 
-  println!("the #{} Fibonacci number is {}", input.unwrap(), result.unwrap());
-  return ExitCode::SUCCESS;
+//   println!("the #{} Fibonacci number is {}", input.unwrap(), result.unwrap());
+//   return ExitCode::SUCCESS;
+// }
+
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+  let ui = AppWindow::new()?;
+
+  let mut count = 0;
+  ui.on_count_button_clicked({
+    let ui_handle = ui.as_weak();
+    move || {
+      let ui = ui_handle.unwrap();
+      println!("DEBUG: count button clicked");
+      count = count + 1; ui.set_count(count);
+    }
+  });
+  ui.on_text_input_enter_pressed(move |text| {
+    println!("DEBUG: text input content is \"{}\"", text);
+  });
+
+  ui.run()
 }
